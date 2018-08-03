b40fe5ba37f144a01567d43a1e654a8549b37002
bdemers1055 <bdemers1055@gmail.com>
2018-08-03 18:17:45 -0400

    get foods object from db

--- index.js
+++ index.js
@@ -12,6 +12,28 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true});
 // setup our port
 const port = process.env.PORT || 2008;
 
+// powere ups / middleware, 
+
+// models
+const Food = mongoose.model('Food', { name:String });
+
+//routes
+//get all foods
+server.get('/foods', async (req, res) => {
+    try { 
+        const foods = await Food.find();
+        res.status(200).json({
+            foods: foods
+        });
+    }
+    catch(err) {
+        res.status(500).json({
+            msg: 'broken'
+        });
+    }
+});
+
+
 // kick it off
 
 server.listen(port, () => { 
